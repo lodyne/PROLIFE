fb8651d3d22687dea35c742e62ccd4df04068d7e
lodyne <lodgmtui@gmail.com>
2020-05-12 09:27:12 +0300

    partial pagination commit

--- prolife/prolife/settings.py
+++ prolife/prolife/settings.py
@@ -32,6 +32,7 @@ ALLOWED_HOSTS = []
 
 INSTALLED_APPS = [
     'blog.apps.BlogConfig',
+    'crispy_forms',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -125,3 +126,5 @@ STATIC_ROOT = os.path.join(BASE_DIR, '')
 STATICFILES_DIRS = [
         os.path.join(BASE_DIR, 'static'),
 ]
+
+CRISPY_TEMPLATE_PACK = 'bootstrap4'
